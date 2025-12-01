973da4081e94669d1788e66b0aac7dbf1c6d8fdd
Gunics Roland <grolcsi09@gmail.com>
2025-12-02 00:24:52 +0100

    fix(2024): crazy c#

---
+++ 2024/D003/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> StartsWithM = new List<string>();
+            string pattern @"";
+
+            StreamReader reader = new StreamReader("input.txt");
+            while(!reader.EndOfStream){
+                string line = reader.ReadLine();
+
+                foreach (string part in line.Split('m'))
+                {
+                    StartsWithM.Add(part);
+                }
+            }
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
